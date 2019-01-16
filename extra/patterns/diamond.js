2e6e16311c35497dbdea6c984abb8b5ceeeaec86
Sagun Saluja <sagun.saluja@successive.tech>
2019-01-16 15:19:05 +0530

    Docs:Create patterns for pratice

---
+++ extra/patterns/diamond.js
@@ -0,0 +1,35 @@
+function diamond(n){
+  let pattern ='';
+  for(let i=1;i<=n;i++)
+  {
+    for(let j=n;j>i;j--)
+    {
+      pattern += ' ';
+    }
+
+    for(let j=1;j<=i;j++)
+    {
+      pattern +='* ';
+    }
+    pattern+='\n'
+
+  }
+  for(let i=n;i>=1;i--)
+  {
+    for(let j=n;j>i;j--)
+    {
+      pattern += ' ';
+    }
+
+
+    for(let j=1;j<=i;j++)
+    {
+      pattern +='* ';
+    }
+    pattern+='\n'
+
+  }
+
+console.log(pattern)
+}
+diamond(5);
