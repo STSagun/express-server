a15e3b2df9dc5570984a0516bf4f333278153633
nikitaagg93 <46665675+nikitaagg93@users.noreply.github.com>
2019-01-22 11:21:55 +0530

    Missed string checking for head trainer constant

--- extraTs/constants.ts
+++ extraTs/constants.ts
@@ -1,7 +1,7 @@
 import { IPermission } from "./interface";
 export const getUsers: string = "getUsers";
 export const getUsers1: string = "getUsers1";
-const headTrainer = "head-trainer";
+const headTrainer: string = "head-trainer";
 const trainer: string = "trainer";
 const trainee: string = "trainee";
 export const permission: IPermission = {
