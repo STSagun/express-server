ad7be47a723c344cb3513aecf2c6f0ad0a3ef21f
Sagun Saluja <sagun.saluja@successive.tech>
2019-01-30 19:19:21 +0530

    feat: update files #40010

--- src/libs/Database.ts
+++ src/libs/Database.ts
@@ -1,11 +1,7 @@
 import * as mongoose from "mongoose";
 
 class Database {
-<<<<<<< HEAD
  static open(mongo) {
-=======
-  static open(mongo) {
->>>>>>> 1bf46a002c994a0c382991f1b255c33bc6959a03
     return new Promise(function(resolve, reject) {
       mongoose
         .connect(
@@ -16,11 +12,7 @@ class Database {
         .catch(err => reject(err));
     });
   }
-<<<<<<< HEAD
   static disconnect() {
-=======
- static disconnect() {
->>>>>>> 1bf46a002c994a0c382991f1b255c33bc6959a03
     mongoose.disconnect();
   }
 }
